2b601b26a159d065ec2b6c5d10e9adca1ef096d5
Nhật Anh <lthanhnha2005@gmail.com>
2026-09-09 14:10:43 +0700

    set up react environment

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+﻿import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>SportCenter</h1>
+      <p>Welcome to SportCenter FE</p>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/utils/helpers.js
@@ -0,0 +1,13 @@
+﻿// Format date to locale string
+export const formatDate = (dateStr) => {
+  if (!dateStr) return ''
+  return new Date(dateStr).toLocaleDateString('vi-VN')
+}
+
+// Format currency to VND
+export const formatCurrency = (amount) => {
+  return new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+  }).format(amount)
+}
